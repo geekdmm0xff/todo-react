07efff48305bf6d63f8b610c45fba9177673b17a
duanmingming <geekdmm@163.com>
2019-03-14 23:36:02 +0800

    add select logic

--- dva-picker/www/app/components/SelectCtrl.js
+++ dva-picker/www/app/components/SelectCtrl.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import classNames from "classnames";
+import * as templateHelper from "../models/templateHelper";
 
 export default class SelectCtrl extends Component {
   constructor(props) {
@@ -23,20 +24,23 @@ export default class SelectCtrl extends Component {
   }
 
   render() {
-    const { type, options, value, title } = this.state;
+    const {
+      data: { type, options, value, title }
+    } = this.props;
     return (
       <td className="select-ctrl" ref="select_ctr_box">
         <div className="menu-selected" onClick={this.handlerSelectedMenu}>
           {title}
         </div>
-        {type === "multi"
-          ? this.renderMultiMenus(options, value)
-          : this.renderSingleMenus(options, value)}
+        {type === "multi" ? this.renderMultiMenus() : this.renderSingleMenus()}
       </td>
     );
   }
   // UI
-  renderSingleMenus = (options, value) => {
+  renderSingleMenus = () => {
+    const { k, tag, updateFunc } = this.props;
+    const { type, options, value, title } = this.state;
+
     let pos = options.indexOf(value);
     return (
       <dl
@@ -45,14 +49,58 @@ export default class SelectCtrl extends Component {
       >
         {options.map((item, index) => (
           <dd className={classNames({ cur: index == pos })} key={index}>
-            <a href="javascript:void(0);">{item}</a>
+            <a
+              href="javascript:void(0);"
+              onClick={() => {
+                this.setState(
+                  {
+                    ...this.state,
+                    show: false,
+                    value: item
+                  },
+                  () => {
+                    console.log("over:", this.state);
+                    updateFunc(k, item, tag, templateHelper.template1);
+                  }
+                );
+              }}
+            >
+              {item}
+            </a>
           </dd>
         ))}
       </dl>
     );
   };
 
-  renderMultiMenus = (options, value) => {
+  renderMultiMenus = () => {
+    const { k, tag, updateFunc } = this.props;
+    const { type, options, value, title } = this.state;
+
+    const addTags = item => {
+      this.setState(
+        {
+          ...this.state,
+          value: [...this.state.value, item]
+        },
+        () => {
+          updateFunc(k, this.state.value, tag, templateHelper.template3);
+        }
+      );
+    };
+
+    const removeTag = item => {
+      this.setState(
+        {
+          ...this.state,
+          value: this.state.value.filter(title => title != item)
+        },
+        () => {
+          updateFunc(k, this.state.value, tag, templateHelper.template3);
+        }
+      );
+    };
+
     return (
       <dl
         className="menu clearfix"
@@ -60,7 +108,18 @@ export default class SelectCtrl extends Component {
       >
         {options.map((item, index) => (
           <dd key={index}>
-            <input type="checkbox" checked={value.includes(item)} />
+            <input
+              type="checkbox"
+              checked={value.includes(item)}
+              onChange={e => {
+                // () =>
+                if (event.target.checked) {
+                  addTags(item);
+                } else {
+                  removeTag(item);
+                }
+              }}
+            />
             {item}
           </dd>
         ))}
