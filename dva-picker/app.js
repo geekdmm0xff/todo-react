588205dea9a84ef84f6c7d484d1fa75357d52635
duanmingming <geekdmm@163.com>
2019-03-20 17:53:13 +0800

    add parse url

--- dva-picker/app.js
+++ dva-picker/app.js
@@ -6,9 +6,47 @@ const url = require("url");
 const fs = require("fs");
 
 app.get("/api", (req, res) => {
-  const { page, pagesize } = url.parse(req.url, true).query;
+  const {
+    id,
+    rate,
+    brand,
+    color,
+    engine,
+    push,
+    control,
+    detail,
+    saler,
+    km,
+    city,
+    page,
+    pagesize,
+    price
+  } = url.parse(req.url, true).query;
 
-  CarSchema.find()
+  const buildPriceQuery = () => {
+    const arr = price.split(",");
+    const priceMin = parseInt(arr[0]);
+    const priceMax = parseInt(arr[1]);
+    return {
+      $gt: priceMin,
+      $lt: priceMax
+    };
+  };
+
+  const buildRateQuery = () => new Array(parseInt(rate)).fill("★").join("");
+
+  const buildQuery = () => {
+    let o = {};
+    id && (o.id = id);
+    rate && (o.rate = buildRateQuery());
+    brand && (o.brand = brand);
+    price && (o.price = buildPriceQuery());
+    return o;
+  };
+  const query = buildQuery();
+  console.log("query:", query);
+
+  CarSchema.find(query)
     .skip((page - 1) * pagesize)
     .limit(parseInt(pagesize))
     .exec((err, result) => {
