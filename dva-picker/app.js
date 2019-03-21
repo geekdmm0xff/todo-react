6562a180b086f6741143a3405c72e65587fd75ba
duanmingming <geekdmm@163.com>
2019-03-21 15:39:49 +0800

    prmise node function

--- dva-picker/app.js
+++ dva-picker/app.js
@@ -14,6 +14,9 @@ app.get("/api", (req, res) => {
     engine,
     push,
     control,
+    name,
+    type,
+    seat,
     buyDate,
     detail,
     saler,
@@ -24,46 +27,87 @@ app.get("/api", (req, res) => {
     price
   } = url.parse(req.url, true).query;
 
-  const buildPriceQuery = () => {
-    const arr = price.split(",");
-    const priceMin = parseInt(arr[0]);
-    const priceMax = parseInt(arr[1]);
+  const buildRangeQuery = range => {
+    const arr = range.split(",");
+    const min = parseInt(arr[0]);
+    const max = parseInt(arr[1]);
     return {
-      $gt: priceMin,
-      $lt: priceMax
+      $gt: min,
+      $lt: max
     };
   };
 
-  const buildRateQuery = () => new Array(parseInt(rate)).fill("★").join("");
+  const buildRateQuery = item => new Array(parseInt(rate)).fill(item).join("");
+
+  const buildMultQuery = v => {
+    return {
+      $in: v.split(",")
+    };
+  };
 
   const buildQuery = () => {
     let o = {};
     id && (o.id = id);
-    rate && (o.rate = buildRateQuery());
+    rate && (o.rate = buildRateQuery("★"));
     brand && (o.brand = brand);
     color && (o.color = color);
-    engine && (o.engine = engine);
+    engine && (o.engine = buildMultQuery(engine));
     push && (o.push = push);
     control && (o.control = control);
+
+    name && (o["system.name"] = name);
+    type && (o["system.type"] = type);
+    seat && (o["system.seat"] = parseInt(seat));
+
     buyDate && (o.buyDate = buyDate);
     detail && (o.detail = detail);
     saler && (o.saler = saler);
     km && (o.km = km);
-    price && (o.price = buildPriceQuery());
+    price && (o.price = buildRangeQuery(price));
     return o;
   };
-  const query = buildQuery();
-  console.log("query:", query);
 
-  CarSchema.find(query)
-    .skip((page - 1) * pagesize)
-    .limit(parseInt(pagesize))
-    .exec((err, result) => {
-      if (!err) res.json({ result });
-      else console.log("err:", err);
+  const fetchCount = query => {
+    return new Promise((resolve, reject) => {
+      CarSchema.count(query, (err, total) => {
+        !err ? resolve(total) : reject(err);
+      });
     });
+  };
+
+  const fetchList = (query, page, pagesize) => {
+    return new Promise((resolve, reject) => {
+      CarSchema.find(query)
+        .skip((page - 1) * pagesize)
+        .limit(parseInt(pagesize))
+        .exec((err, list) => {
+          !err ? resolve(list) : reject(err);
+        });
+    });
+  };
+
+  const flow = async () => {
+    try {
+      const query = buildQuery();
+      console.log("query:", query);
+      let total = await fetchCount(query);
+      let list = await fetchList(query, page, pagesize);
+      res.json({
+        page,
+        total,
+        list
+      });
+    } catch (err) {
+      res.sendStatus(400).json({
+        err
+      });
+    }
+  };
+
+  flow();
 });
 
 mongoose.connect("mongodb://localhost/carsystem");
 app.use(express.static("www"));
 app.listen(3000);
+console.log("server start!");
